42183259ee560e5f30efe37fb9bbec48d97c4e82
Ratthapon Limpapruk <Ratthapon Limpapruk@RATTHAPON>
2024-03-22 17:41:36 +0700

    Add project files.

---
+++ Zteam/Controllers/LibraryController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Zteam.Data;
+using Zteam.Models;
+
+namespace Zteam.Controllers
+{
+    public class libraryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public libraryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Game> game = _db.Game;
+            return View(game);
+        }
+    }
+}
---
+++ Zteam/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Zteam.Models;
+
+namespace Zteam.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Game> Game { get; set; }
+    }
+
+}
