e39c1ac6534e58407c792b7341ebda2277accb40
Ratthapon LIMPAPRUK <ratthapon.l@live.ku.th>
2024-03-23 21:29:40 +0700

    sdsd

--- Zteam/Controllers/HomeController.cs
+++ Zteam/Controllers/HomeController.cs
@@ -28,5 +28,13 @@ namespace Zteam.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult SignUp()
+        {
+            return View();
+        }
     }
 }
